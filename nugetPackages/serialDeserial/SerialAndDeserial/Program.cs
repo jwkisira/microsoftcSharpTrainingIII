07c39c7fbaf81c8986e64e84dd90df79ddcf9ad2
kisira <jwkisira@gmail.com>
2025-04-04 02:37:56 +0300

    apply deserialisation of json string

---
+++ nugetPackages/serialDeserial/SerialAndDeserial/Program.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace Shop{
+    class Product{
+        string Name {get; set;}
+        int Price {get; set;}
+        string Tags {get; set;}
+    }
+
+
+    class Program{
+        public static void Main(string [] args){
+            string json = "{\"Name\": \"Laptop\", \"Price\": 2000, \"Tags\": [\"Electronics\", \"Computers\"]}";
+            
+            Product products = JsonConvert.DeserializeObject<Product>(json);
+        }
+    }
+}
